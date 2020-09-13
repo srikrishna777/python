80f7afc6173ee063a3819b234ae05a3e13973d13
srikrishna777 <70716833+srikrishna777@users.noreply.github.com>
2020-09-13 08:12:46 +0530

    Add files via upload

---
+++ ifstamentsnew.py
@@ -0,0 +1,8 @@
+hot =True
+if hot:
+    print("its hot day")
+    print("drink water")
+else:
+    print("cold day")
+    print("wear warm cloths")
+print("enjoy your day")
---
+++ poundstokilograms.py
@@ -0,0 +1,5 @@
+pounds = float(input("Enter your weight in pounds:"))
+kilo_grams = pounds * 0.453592
+print(pounds , "pounds are equal to" , kilo_grams,"kilograms")
+course = "python for beginner"
+print(course)
